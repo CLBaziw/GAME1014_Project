f68586bdd120f376777d71f7c05e20eb1758d75f
Miaraii25 <65626916+Miaraii25@users.noreply.github.com>
2020-07-09 23:56:45 -0400

    Enemy sprites,and the grounnd is updated (#36)
    Co-authored-by: miarai25 <60987674+miarai25@users.noreply.github.com>

--- SDL_Engine-master/src/PlayScene.cpp
+++ SDL_Engine-master/src/PlayScene.cpp
@@ -171,7 +171,7 @@ void PlayScene::start()
 	m_pPlayer->SetJumping(false);
 
 	//Ground
-	m_ground = new ground(0, 587);
+	m_ground = new ground(0, 588);
 	addChild(m_ground);
 
 	// Bullets
@@ -348,6 +348,7 @@ void PlayScene::MakeObstacles()
 	{
 		m_vec[col]->Update();
 	}
+
 }
 void PlayScene::EnemyShoot()
 {
