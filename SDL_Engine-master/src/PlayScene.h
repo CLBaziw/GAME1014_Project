053b25166d7d7dfc755a04a37fa9a3549cc68975
Miaraii25 <65626916+Miaraii25@users.noreply.github.com>
2020-08-07 22:18:52 -0400

    Ground,jump,Enemyhealth (#60)
    * Remove redundant asset
    
    * Ground3,jump,enemyhealth
    
    Co-authored-by: Christina Baziw <cbaziw@outlook.com>
    Co-authored-by: Johnny Diep <longdiep10102001@gmail.com>
    Co-authored-by: Diep, Ly Bao Long <60988318+JohnnyDiep1021@users.noreply.github.com>

--- SDL_Engine-master/src/PlayScene.h
+++ SDL_Engine-master/src/PlayScene.h
@@ -37,6 +37,16 @@ public:
 	void gameOver();
 	void ReadObstacleFile();
 	void BulletCheck(int i, int score);
+
+	// shoot Timer
+	float ShootTimer;
+	void CanShoot();
+	// player jump function;
+	bool playerJumping;
+	void PlayerJump();
+	void playerJumpTimer();
+	float playerjumpTime;
+	float Jumpforce;
 	//void moveBackground();
 
 private:
@@ -83,7 +93,7 @@ private:
 		m_timerMax = 100;
 	float x;
 	float y;
-	int cooldown_specialskill =0;
+	int cooldown_specialskill = 0;
 };
 
 #endif /* defined (__PLAY_SCENE__) */
