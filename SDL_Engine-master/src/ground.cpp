e3ff560b91231a975eb5194112bc8f97cb123c50
Christina Baziw <cbaziw@outlook.com>
2020-08-09 09:49:35 -0400

    trying to merge again, see if the merge was the problem (#64)
    * Remove redundant asset
    
    * Ground3,jump,enemyhealth
    
    Co-authored-by: Johnny Diep <longdiep10102001@gmail.com>
    Co-authored-by: Diep, Ly Bao Long <60988318+JohnnyDiep1021@users.noreply.github.com>
    Co-authored-by: Miaraii25 <65626916+Miaraii25@users.noreply.github.com>
    Co-authored-by: Miaraii25 <Mariam.Ogunlesi@georgebrown.ca>

--- SDL_Engine-master/src/ground.cpp
+++ SDL_Engine-master/src/ground.cpp
@@ -8,10 +8,14 @@ ground::ground(float x, float y)
 	{
 		TextureManager::Instance()->load("../Assets/plat/ground.png", "ground");
 	}
-	else
+	else if (TheGame::Instance()->getLevel() == 1)
 	{
 		TextureManager::Instance()->load("../Assets/plat/ground2.png", "ground");
 	}
+	else
+	{
+		TextureManager::Instance()->load("../Assets/plat/ground3.png", "ground");
+
 
 	auto size = TextureManager::Instance()->getTextureSize("ground");
 	setWidth(size.x);
@@ -45,4 +49,9 @@ void ground::update()
 void ground::clean()
 {
 }
+
+//SDL_FRect& ground::getDst()
+//{
+//	// TODO: insert return statement here
+//}
 
